726c8466a0e1bae649f4154979a645c0987b776f
Vera Nouaime <veranouaime@Veras-MacBook-Air.local>
2023-09-05 22:31:41 -0400

    Default Image URL not displaying

--- app.py
+++ app.py
@@ -2,13 +2,14 @@
 
 from flask import Flask, render_template, request, redirect, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.app_context().push()
 
 debug = DebugToolbarExtension(app)
 
@@ -17,4 +18,40 @@ db.create_all()
 
 @app.route("/")
 def home():
-    return "Hello!"
+    """ Redirects user to all users page """
+
+    return redirect("/users")
+
+@app.route("/users")
+def all_users(): 
+    """ Displays list of all users """
+    users = User.query.all()
+
+    return render_template("base.html", users=users)
+
+@app.route("/users/new")
+def users_new_form(): 
+    """ Displays form to create a new user """
+
+    return render_template("new_user_form.html")
+
+@app.route("/users/new", methods=["POST"])
+def users_new(): 
+    """ Handles POST request for new user """ 
+
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    image_url = request.form["image_url" or None]
+
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/users")
+
+@app.route("/users/<int:user_id>")
+def user_page(user_id): 
+
+    user = User.query.get_or_404(user_id)
+    return render_template("user_page.html", user=user)
--- models.py
+++ models.py
@@ -17,5 +17,5 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(50), nullable=False, unique=True)
     last_name = db.Column(db.String(50), nullable=False, unique=True)
-    image_url = db.Column(db.String(255), nullable=False, default="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fno-image-available&psig=AOvVaw3MW4bhjVsEqe4olMGfEe3z&ust=1693110580818000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCIDL08e--YADFQAAAAAdAAAAABAE")
+    image_url = db.Column(db.String(255), nullable=False, default="https://icon-library.com/images/no-picture-available-icon/no-picture-available-icon-20.jpg")
 
